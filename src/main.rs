215111b023cd4c7161de0d72be0eba1f899e8588
Alvaro Carranza <alvarocarranzacarrion@gmail.com>
2025-12-05 19:11:16 +0100

    replaced synchronous call to fetch_all_links() with a tokio runtime som ain remains synchronous for callers

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use sppd_cli::{cli, downloader, errors};
 use errors::AppResult;
 
 fn main() -> AppResult<()> {
-    let (minor_contracts_links, public_tenders_links) = downloader::fetch_all_links()?;
+    let rt = tokio::runtime::Runtime::new()
+        .map_err(|e| errors::AppError::IoError(e.to_string()))?;
+
+    let (minor_contracts_links, public_tenders_links) = rt.block_on(downloader::fetch_all_links())?;
     cli::cli(&minor_contracts_links, &public_tenders_links)?;
     Ok(())
 }
