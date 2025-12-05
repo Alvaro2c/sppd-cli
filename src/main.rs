ad390d4278752791650c40fb0cf4d15bdb7a8c4a
Alvaro Carranza <alvarocarranzacarrion@gmail.com>
2025-12-05 18:59:33 +0100

    Passing reference to cli call

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use errors::AppResult;
 
 fn main() -> AppResult<()> {
     let (minor_contracts_links, public_tenders_links) = downloader::fetch_all_links()?;
-    cli::cli(minor_contracts_links, public_tenders_links)?;
+    cli::cli(&minor_contracts_links, &public_tenders_links)?;
     Ok(())
 }
