a408e683e57d644999891723c68c60147a68a1ab
Alvaro Carranza <alvarocarranzacarrion@gmail.com>
2025-12-07 20:30:01 +0100

    main with tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-use errors::AppResult;
-use sppd_cli::{cli, downloader, errors};
+use sppd_cli::{cli, downloader, errors::AppResult};
 
-fn main() -> AppResult<()> {
-    let rt =
-        tokio::runtime::Runtime::new().map_err(|e| errors::AppError::IoError(e.to_string()))?;
-
-    let (minor_contracts_links, public_tenders_links) =
-        rt.block_on(downloader::fetch_all_links())?;
-    cli::cli(&minor_contracts_links, &public_tenders_links)?;
+#[tokio::main]
+async fn main() -> AppResult<()> {
+    let (minor_contracts_links, public_tenders_links) = downloader::fetch_all_links().await?;
+    cli::cli(&minor_contracts_links, &public_tenders_links).await?;
     Ok(())
 }
